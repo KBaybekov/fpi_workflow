bf95f9f2fabd3b23cb6d0d76c222bb5cec65d199
KBaybekov <k.baybekov@gmail.com>
2024-12-10 13:14:03 +0300

    updated ref genomes list

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -55,8 +55,8 @@ def parse_cli_args():
     # Если мы выравниваем NTM на реф геном, то нам нужно заменить в аргументах default ref на ref нужного вида.
     # Для этого снабжаем пайплайн словарём с указанием id и превалирующего в нём вида
     if any(['ntm_ref' in module for module in args.modules]):
-        ntm_list = load_yaml(file_path=f'{configs}/ntm_list.yaml')
-        ref_list = load_yaml(file_path=f'{configs}/species_ref_genomes.yaml')
+        ntm_list = load_yaml(file_path=f'{configs}/gcfs_ntm_list.yaml')
+        ref_list = load_yaml(file_path=f'{configs}/reference_genomes_myco.yaml')
         reversed_ref_list = {val[0]:key for key,val in ref_list.items()}
         refs4ids = {key: ref_list[val] for key,val in ntm_list.items()}
         species4ids = {key: reversed_ref_list[val[0]] for key,val in refs4ids.items()}
